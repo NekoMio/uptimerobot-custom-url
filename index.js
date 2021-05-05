512e911a06bd4cf4e217d174571b7931ca2ae3ad
arimeisels3@gmail.com <me@arimeisels.com>
2021-05-05 19:27:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express"),
       app = express(),
+      cors = require("cors"),
       request = require("request");
 
-app.get("/:id", (req, res) => {
+app.get("/:id", cors(), (req, res) => {
     request.get(`https://stats.uptimerobot.com/${req.params.id}`, (error, response, body) => {
-        res.send(body.replace(/https:\/\/stats.uptimerobot.com\/api/g, `https://${req.get("host")}/get/https://stats.uptimerobot.com/api`));
+        body = body.replace(/https:\/\/stats.uptimerobot.com\/(?=<req.params.id>)/g, `https://${req.get("host")}/get/https://stats.uptimerobot.com/${req.params.id}`);
+        body = body.replace(/https:\/\/stats.uptimerobot.com\/api/g, `https://${req.get("host")}/get/https://stats.uptimerobot.com/api`);
+        res.send(body);
     });
 });
 
-app.get("/get/*", (req, res) => {
+app.get("/get/*", cors(), (req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     request.get(req.url.split("/get/")[1], (error, response, body) => {
         if (error) return res.send(error);
